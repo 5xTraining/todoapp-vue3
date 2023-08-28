8e1b4309d5fbe47afa7003c678fa2290e881ce6e
Eddie Kao <eddie@5xcampus.com>
2023-08-29 00:01:45 +0800

    cc

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
-<script setup></script>
+<script setup>
+import TodoItem from "./components/TodoItem.vue"
+</script>
 
-<template></template>
+<template>
+  <main class="container mx-auto">
+    <header class="m-2">
+      <h1 class="text-6xl font-thin select-none">TODO!</h1>
+      <div class="font-semibold select-none text-neutral-600">simple and studid todo app</div>
+    </header>
+    <form class="px-10 py-12 bg-white shadow-sm">
+      <section class="flex">
+        <input type="text" placeholder="做點重要的事吧..." class="w-full text-2xl focus:outline-none input-lg input input-bordered" />
+        <button class="text-xl btn-lg btn btn-neutral">新增</button>
+      </section>
+    </form>
+
+    <section class="px-10 py-6 mt-4 bg-white">
+      <ul class="">
+        <TodoItem />
+      </ul>
+    </section>
+  </main>
+</template>
 
 <style scoped></style>
